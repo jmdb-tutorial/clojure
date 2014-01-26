30b4b8fe062714a3f51dcaebe59febca324deb01
Jim Barritt <jim@planet-ix.com>
2014-01-26 23:46:36 +0000

    [jmdb] Adding some tweaks

--- structure/src/structure/destructuring.clj
+++ structure/src/structure/destructuring.clj
@@ -12,8 +12,8 @@
 ;; user> (f2 1 2 3 4)
 ;; "a= 1 args= (2 3 4)"
 
-(defn f3 [& [a & args :as all]]
-  (str "a= " a " args= " args " all= " all))
+(defn f3 [& [a & args :as allargs]]
+  (str "a= " a " args= " args " all= " allargs))
 
 ;; user> (f3 1 2 3 4)
 ;; "a= 1 args= (2 3 4) all= (1 2 3 4)"
@@ -24,3 +24,32 @@
   ([a & args]
      (str "a= " a " args= " args)))
 
+;; user> (f4)
+;; "no-args"
+;; user> (f4 1 2 3 4)
+;; "a= 1 args= (2 3 4)"
+
+(defn f5 [& [a & args :as allargs]]
+  (if (or (empty? allargs) (= 1 a))
+        (str "Either there were 0 args, or a=1")
+        (str "a= " a " args= " args " all= " allargs)))
+
+;; user> (f5)
+;; "Either there were 0 args, or a=1"
+;; user> (f5 1 2 3 4)
+;; "Either there were 0 args, or a=1"
+;; user> (f5 :foo 2 3 4)
+;; "a= :foo args= (2 3 4) all= (:foo 2 3 4)"
+
+(defn f6 [& [a & args :as allargs]]
+  (cond (or (empty? allargs) (= 1 a))
+        (str "Either there were 0 args, or a=1")
+        :otherwise
+        (str "a= " a " args= " args " all= " allargs)))
+
+;; user> (f6)
+;; "Either there were 0 args, or a=1"
+;; user> (f6 1 2 3 4)
+;; "Either there were 0 args, or a=1"
+;; user> (f6 :foo 2 3 4)
+;; "a= :foo args= (2 3 4) all= (:foo 2 3 4)"
